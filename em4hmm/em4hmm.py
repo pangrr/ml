212a4114290b45dda8e48a97d2a8700adc354aa5
pangran <pangrr89@gmail.com>
2015-04-16 09:32:55 -0400

    update

--- em4hmm/em4hmm.py
+++ em4hmm/em4hmm.py
@@ -73,19 +73,19 @@ class Gauss:
 class HMM:
 
     # Initialize the HMM by generating its components.
-    def __init__ (self, dataSet, nComp, it):
+    def __init__ (self, trainData, devData, nComp, it):
         # Constant fields. #
 
         # Maximum number of iterations for EM training.
         self.it = it
         # Training data for EM training.
-        self.dataSet = dataSet
+        self.trainData = trainData
+        # Developing data.
+        self.devData = devData
         # Number of dimensions.
-        self.dim = dataSet.dim
+        self.dim = trainData.dim
         # Number of components.
         self.nComp = nComp
-        # Number of training data points.
-        self.nData = dataSet.nData
 
 
         # Parameters of HMM. #
@@ -104,19 +104,25 @@ class HMM:
         # Intermediate fields. #
 
         # A matrix of marginal posterior distribution of latent variables computed and updated at every E step.
-        self.margin = np.zeros ([nComp, self.nData])
+        self.margin = np.zeros ([nComp, self.trainData.nData])
         # A matrix of joint posterior distribution of two successive latent variables, computed and updated at each E step.
-        self.joint = np.zeros ([nComp, nComp, self.nData-1])
-        # A matrix computed in the forward procedure.
-        self.fwd = np.zeros ([nComp, self.nData])
-        # A matrix computed in the backward procedure.
-        self.bwd = np.zeros ([nComp, self.nData])
+        self.joint = np.zeros ([nComp, nComp, self.trainData.nData-1])
+        # A matrix computed in the forward procedure for train data.
+        self.fwd = np.zeros ([nComp, self.trainData.nData])
+        # A matrix computed in the forward procedure for the dev data.
+        self.devfwd = np.zeros([nComp, self.devData.nData])
+        # A matrix computed in the backward procedure for training data.
+        self.bwd = np.zeros ([nComp, self.trainData.nData])
         # A list representing the adjustment coefficient for fwd and bwd.
-        self.adj = np.zeros (self.nData)
+        self.adj = np.zeros (self.trainData.nData)
+        # A list representing the adjustment coefficient for fwd and bwd on dev data.
+        self.devadj = np.zeros(self.devData.nData)
 
 
-        # A list of likelihood on training data computed for each iteration during the training.
-        self.likeList = []
+        # A list of log likelihood on training data computed for each iteration during the training.
+        self.trainLike = []
+        # A list of log likelihood on developing data computed for each iteration during the training.
+        self.devLike = []
 
 
 
@@ -155,15 +161,15 @@ class HMM:
     def randData (self):
         select = []
         for i in range (self.nComp):
-            #select.append (self.dataSet.data[random.randint (0, self.dataSet.nData-1)])
-            select.append (self.dataSet.data[i])
+            select.append (self.trainData.data[random.randint (0, self.trainData.nData-1)])
+            #select.append (self.trainData.data[i])
         return select
 
 
 
     # Compute and return the covariance of the whole training data.
     def initCov (self):
-        return np.cov (self.dataSet.data.T)
+        return np.cov (self.trainData.data.T)
 
 
     def train (self):
@@ -171,7 +177,6 @@ class HMM:
             print ("Training...%d/%d" % (i+1, self.it), end="\r")
             self.EStep ()
             self.MStep ()
-            self.like ()
         print ()
 
 
@@ -183,22 +188,23 @@ class HMM:
 
     # Use forward and backward algorithm to compute marginal distribution and joint distribution.
     def EStep (self):
-        self.forward ()
+        self.forward (self.trainData, self.fwd, self.adj)
         self.backward ()
+        self.like(self.adj, self.trainLike)
+        self.forward(self.devData, self.devfwd, self.devadj)
+        self.like(self.devadj, self.devLike)
         self.updateMargin ()
         self.updateJoint ()
 
 
+
     # Forward procedure.
-    def forward (self):
-        fwd = self.fwd
+    def forward (self, dataSet, fwd, adj):
         init = self.init
         comp = self.comp
         nComp = self.nComp
         tran = self.tran
-        dataSet = self.dataSet
         nData = dataSet.nData
-        adj = self.adj
 
         adj[0]= 0.0
         for i in range (nComp):
@@ -226,10 +232,9 @@ class HMM:
 
 
 
-
     # Backward procedure.
     def backward (self):
-        dataSet = self.dataSet
+        dataSet = self.trainData
         nData = dataSet.nData
         bwd = self.bwd
         nComp = self.nComp
@@ -245,7 +250,7 @@ class HMM:
                 bwd[i][t] = 0.0
                 for j in range (nComp):
                     bwd[i][t] += bwd[j][t+1] * tran[i][j] * comp[j].prob(dataSet.data[t+1])
-                bwd[i][t] /= adj[t]
+                bwd[i][t] /= adj[t+1]
 
 
 
@@ -262,12 +267,9 @@ class HMM:
         fwd = self.fwd
         bwd = self.bwd
 
-        for t in range (self.nData):
+        for t in range (self.trainData.nData):
             for i in range (self.nComp):
-                margin[i][t] = 0.0
-                for j in range (self.nComp):
-                    margin[i][t] += fwd[j][t] * bwd[j][t]
-                margin[i][t] = fwd[i][t] * bwd[i][t] / margin[i][t]
+                margin[i][t] = fwd[i][t] * bwd[i][t]
 
 
 
@@ -276,20 +278,18 @@ class HMM:
     def updateJoint (self):
         joint = self.joint
         fwd = self.fwd
+        adj = self.adj
         bwd = self.bwd
         tran = self.tran
         nComp = self.nComp
-        dataSet = self.dataSet
+        dataSet = self.trainData
         comp = self.comp
 
         for t in range (dataSet.nData-1):
             dataPoint = dataSet.data[t+1]
             for i in range (nComp):
-                for j in range (nComp):
-                    joint[i][j][t] = 0.0
-                    for k in range (nComp):
-                        joint[i][j][t] += fwd[k][t] * bwd[k][t]
-                    joint[i][j][t] = fwd[i][t] * tran[i][j] * bwd[j][t+1] * comp[j].prob(dataPoint) / joint[i][j][t]
+                for j in range(nComp):
+                    joint[i][j][t] = fwd[i][t] * comp[i].prob(dataPoint) * tran[i][j] * bwd[i][t+1] / adj[t+1]
 
 
 
@@ -300,6 +300,9 @@ class HMM:
         self.updateTran ()
         self.updateComp ()
 
+        #for gauss in self.comp:
+            #print (gauss.cov)
+
 
 
 
@@ -307,8 +310,10 @@ class HMM:
     # Compute and update initial state distribution.
     def updateInit (self):
         for i in range (self.nComp):
-            self.init[i] = self.margin[i][0]
-
+            self.init[i] = 0.0
+            for j in range (self.nComp):
+                self.init[i] += self.margin[j][0]
+            self.init[i] = self.margin[i][0] / self.init[i]
 
 
 
@@ -318,11 +323,15 @@ class HMM:
         for i in range (self.nComp):
             for j in range (self.nComp):
                 sum1 = 0.0
-                sum2 = 0.0
-                for t in range (self.nData-1):
+                for t in range (self.trainData.nData-1):
                     sum1 += self.joint[i][j][t]
-                    sum2 += self.margin[i][t]
+                sum2 = 0.0
+                for k in range (self.nComp):
+                    for t in range (self.trainData.nData-1):
+                        sum2 += self.joint[i][k][t]
                 self.tran[i][j] = sum1 / sum2
+
+
 
 
 
@@ -344,7 +353,7 @@ class HMM:
         for i in range (self.nComp):
             gauss = self.comp[i]
             gauss.freq = 0.0
-            for t in range (self.dataSet.nData):
+            for t in range (self.trainData.nData):
                 gauss.freq += self.margin[i][t]
 
 
@@ -354,7 +363,7 @@ class HMM:
 
     # Compute and update mean of given gaussian distribution.
     def updateGaussMean (self, i):
-        dataSet = self.dataSet
+        dataSet = self.trainData
         margin = self.margin
         gauss = self.comp[i]
 
@@ -367,7 +376,7 @@ class HMM:
     # Compute and update cov of given gaussian distribution.
     def updateGaussCov (self, i):
         dim = self.dim
-        dataSet = self.dataSet
+        dataSet = self.trainData
         margin = self.margin
         gauss = self.comp[i]
         sum = np.zeros ([self.dim, self.dim])
@@ -383,17 +392,12 @@ class HMM:
 
 
     # Compute and add to record the log likelihood on given data set.
-    def like (self):
-        self.likeList.append (math.log(self.adj[self.nData-1]))
+    def like (self, adj, likeList):
+        logLike = 0.0
+        for i in adj:
+            logLike += math.log(i)
 
-
-
-
-
-
-
-
-
+        likeList.append(logLike)
 
 
 
@@ -414,26 +418,38 @@ class HMM:
 
 # Plot the log likelihood of train data and dev data over iterations for HMMs of different number of components (latent states).
 
-def test (dataSet,  minNComp, maxNComp, it):
+def test (trainData, devData, minNComp, maxNComp, it):
     nComps = list (range(minNComp, maxNComp+1))
 
     for nComp in range (minNComp, maxNComp+1):
         print ("\nTesting...%d/%d" % (nComp-minNComp+1, maxNComp-minNComp+1))
-        hmm = HMM (dataSet, nComp, it)
+        hmm = HMM (trainData, devData, nComp, it)
         hmm.train ()
 
-        plt.plot (hmm.likeList)
-        plt.savefig (str(nComp))
+        fig = plt.figure()
+        plt.title("Log Likelihood for " + str(nComp) + " Hidden States")
+        ax1 = fig.add_subplot(111)
+        ax1.set_xlabel("Iterations")
+        ax1.set_ylabel("Train Data Log Likelihood")
+        ax1.plot(hmm.trainLike, label="train", color="red")
+        ax1.legend(loc=3)
+        ax2 = ax1.twinx()
+        ax2.set_ylabel("Dev Data Log Likelihood")
+        ax2.plot(hmm.devLike, label="dev")
+        ax2.legend(loc=4)
+        plt.savefig(str(nComp))
         plt.clf ()
+
 
 
 
 
 if __name__ == "__main__":
     print ("Reading data...", end="")
-    dataSet = DataSet ("train.dat")
-    print ("Done.")
-    test (dataSet, 3, 3, 20)
+    trainData = DataSet("train.dat")
+    devData = DataSet("dev.dat")
+    print("Done.")
+    test(trainData, devData, 1, 5, 10)
 
 
 
